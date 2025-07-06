ed2ca3878742639758550d26d446729b79069c74
zichemerr <zichemerr9@gmail.com>
2025-07-06 14:30:12 +0500

    Update LevelsConfig.cs

--- Assets/Sources/Code/Configs/LevelsConfig.cs
+++ Assets/Sources/Code/Configs/LevelsConfig.cs
@@ -1,16 +1,17 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 [CreateAssetMenu(fileName = nameof(LevelsConfig), menuName = "Configs/" + nameof(LevelsConfig), order = 0)]
 public class LevelsConfig : ScriptableObject
 {
-    [SerializeField] private List<Level> _level;
+    [SerializeField] private List<Level> _levels;
 
-    public int LevelCount => _level.Count;
+    public int LevelCount => _levels.Count;
     
     public Level GetLevelPrefabByIndex(int levelNumber)
     {
-        return _level[levelNumber];
+        return _levels[levelNumber];
     }
 }
 
